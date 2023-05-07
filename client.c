002db28c70cfde860b3d10a71a8c73b677486211
aanacarolina <aanacarolina@gmail.com>
2023-05-07 14:12:32 -0300

    impriving messages

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: anacaro3 <anacaro3@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 20:19:50 by anacaro3          #+#    #+#             */
-/*   Updated: 2023/05/07 11:55:45 by anacaro3         ###   ########.fr       */
+/*   Updated: 2023/05/07 14:12:20 by anacaro3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	g_bit;
 
-void	received(int sig)
+void	msg_received(int sig)
 {
 	if (sig == SIGUSR1)
 		g_bit++;
@@ -77,12 +77,12 @@ int	main(int argc, char **argv)
 {
 	pid_t	pid;
 
-	signal(SIGUSR1, received);
-	signal(SIGUSR2, received);
+	signal(SIGUSR1, msg_received);
+	signal(SIGUSR2, msg_received);
 	if (argc != 3)
 		error("ArgumentError");
 	if (check_digit(argv[1]) || ft_strlen(argv[1]) > 7)
-		error("Error PID..");
+		error("Wrong PID\n");
 	pid = ft_atoi(argv[1]);
 	send_char(pid, argv[2]);
 	send_char(pid, "\n");
--- minitalk.h
+++ minitalk.h
@@ -6,17 +6,18 @@
 /*   By: anacaro3 <anacaro3@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 11:44:25 by anacaro3          #+#    #+#             */
-/*   Updated: 2023/05/07 11:44:26 by anacaro3         ###   ########.fr       */
+/*   Updated: 2023/05/07 14:08:12 by anacaro3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <stdio.h> //this header is used for the printf() function.
-# include <unistd.h> //this header is used for the write() function.
-# include <stdlib.h> //this header is used for the malloc() function.
-# include <signal.h> //this header is used for the signal() function.
-# include <sys/types.h> //this header is used for the kill() function.
-# include "libft/libft.h" //this i
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <sys/types.h>
+# include "libft/libft.h"
+
 #endif
